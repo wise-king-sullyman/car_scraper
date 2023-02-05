dc4ce0e882f8c4175fdfc64137f62683e7504e90
Sully <wise.king.sullyman@gmail.com>
2023-02-05 11:25:35 -0500

    Fix routes

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const app = express();
 const port = 80;
 
 function getReportIndex(make) {
-  return readdirSync(path.join(process.cwd(), `backend/data/${make}`));
+  return readdirSync(path.join(process.cwd(), `data/${make}`));
 }
 
 function getReport({ make, reportId }) {
   const reportName = getReportIndex(make)[reportId]
-  return path.join(process.cwd(), `backend/data/${make}/${reportName}`);
+  return path.join(process.cwd(), `data/${make}/${reportName}`);
 }
+
+app.get("/", (req, res) => {
+  res.send('hello world');
+});
 
 app.get("/:make", (req, res) => {
   res.send(getReportIndex(req.params.make));
